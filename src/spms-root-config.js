2b504a92f6ed8ad1d6fb20681c94e3d901b0d5db
Anderson Melo <anderson@stalo.digital>
2023-04-19 17:18:27 +0100

    Adjust to receive the applications

--- src/spms-root-config.js
+++ src/spms-root-config.js
@@ -7,14 +7,17 @@ import {
 import microfrontendLayout from "./microfrontend-layout.html";
 
 const routes = constructRoutes(microfrontendLayout);
+
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
     return System.import(name);
   },
 });
+
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
 applications.forEach(registerApplication);
 layoutEngine.activate();
+
 start();
